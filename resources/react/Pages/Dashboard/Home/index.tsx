75279d940f212309a07a027a707ae05c5ca5df06
Youssef Bibawy <122090895+dsc-youssef@users.noreply.github.com>
2023-10-18 10:33:52 +0300

    fix font style

--- resources/react/Pages/Dashboard/Home/index.tsx
+++ resources/react/Pages/Dashboard/Home/index.tsx
@@ -7,7 +7,7 @@ import DashboardLayout from "@/Layout/Dashboard";
 const Home: FC = () => {
   return (
     <DashboardLayout pageTitle="Home">
-      <></>
+      <>it's home</>
     </DashboardLayout>
   )
 }
